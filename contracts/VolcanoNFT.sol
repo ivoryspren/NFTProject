0605a19e011128ff41967ddf8fc192e65e46ecf5
Michelle Shelby <michelleshelby76@gmail.com>
2022-11-06 20:00:29 -0600

    added token URI to mint function

--- contracts/VolcanoNFT.sol
+++ contracts/VolcanoNFT.sol
@@ -3,11 +3,11 @@
 pragma solidity ^0.8.4;
 
 import "hardhat/console.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract VolcanoNFT is ERC721, Ownable {
+contract VolcanoNFT is ERC721URIStorage, Ownable {
 
     using Counters for Counters.Counter;
 
@@ -16,13 +16,16 @@ contract VolcanoNFT is ERC721, Ownable {
     constructor() ERC721("VolcanoNFT", "VCO") {
     }
 
-    function safeMint(address to) public onlyOwner {
+    function mintNFT(address to, string memory tokenURI) public onlyOwner returns (uint256){
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
+        _setTokenURI(tokenId, tokenURI);
+
+        return tokenId;
     }
 
-    function safeTransfer(address from, address to, uint256 tokenId) public {
+    function transferNFT(address from, address to, uint256 tokenId) public {
         _safeTransfer(from, to, tokenId, "");
     }
 }
