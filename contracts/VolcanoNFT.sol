a5aaef3960fca961314d5326702a2646396e0fc7
Michelle Shelby <michelleshelby76@gmail.com>
2022-11-19 10:11:13 -0600

    updated mintNFT functions to require payment

--- contracts/VolcanoNFT.sol
+++ contracts/VolcanoNFT.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract VolcanoNFT is ERC721URIStorage, Ownable {
 
@@ -16,22 +16,24 @@ contract VolcanoNFT is ERC721URIStorage, Ownable {
     constructor() ERC721("VolcanoNFT", "VCO") {
     }
 
-    function mintNFT(address to, string memory tokenURI) public onlyOwner returns (uint256){
+    function mintNFT(address to, string memory tokenURI) public onlyOwner{
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, tokenURI);
-
-        return tokenId;
     }
 
-    function mintNFTpublic(address to, string memory tokenURI) public payable returns (uint256) {
+    function mintNFTpublic(address to, string memory tokenURI) public payable {
+        require(
+            msg.value >= 10000000000000000,
+            "The transfer amount must be more than 0.01 ETH."
+        );
+        payable(owner()).transfer(msg.value);
+
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, tokenURI);
-
-        return tokenId;
     }
 
     function transferNFT(address from, address to, uint256 tokenId) public {
